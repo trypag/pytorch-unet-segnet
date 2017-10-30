5a144b3a5fbd3d0b727472fab8315393c22d08e6
Pierre-Antoine Ganaye <ganaye@iv-ms-593.creatis.insa-lyon.fr>
2017-10-30 16:46:25 +0100

    update models

--- models/segnet.py
+++ models/segnet.py
@@ -2,6 +2,61 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class SegNet(nn.Module):
+    """SegNet: A Deep Convolutional Encoder-Decoder Architecture for
+    Image Segmentation. https://arxiv.org/abs/1511.00561
+    See https://github.com/alexgkendall/SegNet-Tutorial for original models.
+
+    Args:
+        num_classes (int): number of classes to segment
+        n_init_features (int): number of input features in the fist convolution
+        drop_rate (float): dropout rate of each encoder/decoder module
+        filter_config (list of 5 ints): number of output features at each level
+    """
+    def __init__(self, num_classes, n_init_features=1, drop_rate=0.5,
+                 filter_config=(64, 128, 256, 512, 512)):
+        super(SegNet, self).__init__()
+
+        self.encoders = []
+        self.decoders = []
+        # setup number of conv-bn-relu blocks per module and number of filters
+        encoder_n_layers = (2, 2, 3, 3, 3)
+        encoder_filter_config = (n_init_features,) + filter_config
+        decoder_n_layers = (3, 3, 3, 2, 1)
+        decoder_filter_config = filter_config[::-1] + (filter_config[0],)
+
+        for i in range(0, 5):
+            # encoder architecture
+            self.encoders.append(_Encoder(encoder_filter_config[i],
+                                          encoder_filter_config[i + 1],
+                                          encoder_n_layers[i], drop_rate))
+
+            # decoder architecture
+            self.decoders.append(_Decoder(decoder_filter_config[i],
+                                          decoder_filter_config[i + 1],
+                                          decoder_n_layers[i], drop_rate))
+
+        # final classifier (equivalent to a fully connected layer)
+        self.classifier = nn.Conv2d(filter_config[0], num_classes, 3, 1, 1)
+
+    def forward(self, x):
+        indices = []
+        unpool_sizes = []
+        feat = x
+
+        # encoder path, keep track of pooling indices and features size
+        for i in range(0, 5):
+            (feat, ind), size = self.encoders[i](feat)
+            indices.append(ind)
+            unpool_sizes.append(size)
+
+        # decoder path, upsampling with corresponding indices and size
+        for i in range(0, 5):
+            feat = self.decoders[i](feat, indices[4 - i], unpool_sizes[4 - i])
+
+        return self.classifier(feat)
+
+
 class _Encoder(nn.Module):
     def __init__(self, n_in_feat, n_out_feat, n_blocks=2, drop_rate=0.5):
         """Encoder layer follows VGG rules + keeps pooling indices
@@ -68,58 +123,3 @@ class _Decoder(nn.Module):
     def forward(self, x, indices, size):
         unpooled = F.max_unpool2d(x, indices, 2, 2, 0, size)
         return self.features(unpooled)
-
-
-class SegNet(nn.Module):
-    """SegNet: A Deep Convolutional Encoder-Decoder Architecture for
-    Image Segmentation. https://arxiv.org/abs/1511.00561
-    See https://github.com/alexgkendall/SegNet-Tutorial for original models.
-
-    Args:
-        num_classes (int): number of classes to segment
-        n_init_features (int): number of input features in the fist convolution
-        drop_rate (float): dropout rate of each encoder/decoder module
-        filter_config (list of 5 ints): number of output features at each level
-    """
-    def __init__(self, num_classes, n_init_features=1, drop_rate=0.5,
-                 filter_config=(64, 128, 256, 512, 512)):
-        super(SegNet, self).__init__()
-
-        self.encoders = []
-        self.decoders = []
-        # setup number of conv-bn-relu blocks per module and number of filters
-        encoder_n_layers = (2, 2, 3, 3, 3)
-        encoder_filter_config = (n_init_features,) + filter_config
-        decoder_n_layers = (3, 3, 3, 2, 1)
-        decoder_filter_config = filter_config[::-1] + (filter_config[0],)
-
-        for i in range(0, 5):
-            # encoder architecture
-            self.encoders.append(_Encoder(encoder_filter_config[i],
-                                          encoder_filter_config[i + 1],
-                                          encoder_n_layers[i], drop_rate))
-
-            # decoder architecture
-            self.decoders.append(_Decoder(decoder_filter_config[i],
-                                          decoder_filter_config[i + 1],
-                                          decoder_n_layers[i], drop_rate))
-
-        # final classifier (equivalent to a fully connected layer)
-        self.classifier = nn.Conv2d(filter_config[0], num_classes, 3, 1, 1)
-
-    def forward(self, x):
-        indices = []
-        unpool_sizes = []
-        feat = x
-
-        # encoder path, keep track of pooling indices and features size
-        for i in range(0, 5):
-            (feat, ind), size = self.encoders[i](feat)
-            indices.append(ind)
-            unpool_sizes.append(size)
-
-        # decoder path, upsampling with corresponding indices and size
-        for i in range(0, 5):
-            feat = self.decoders[i](feat, indices[4 - i], unpool_sizes[4 - i])
-
-        return self.classifier(feat)
